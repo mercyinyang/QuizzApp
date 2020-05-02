a990aa8b41b063eb7e0725a65a35b786b7776337
mercyinyang <mercinyang@gmail.com>
2020-05-02 19:15:00 +0100

    New files

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+var option_1 = document.getElementById('option_1');
+var option_2 = document.getElementById('option_2');
+var option_3 = document.getElementById('option_3');
+var option_4 = document.getElementById('option_4');
+
+    var app{
+        questions:[
+            {q:'1. What is the longest river in Africa?', options: ['River Niger','River Nile','River Missisipi','River Chad'], answer:1}
+
+            {q:'1. What is the longest river in Africa?', options: ['River Niger','River Nile','River Missisipi','River Chad'], answer:1}
+
+            {q:'1. What is the longest river in Africa?', options: ['River Niger','River Nile','River Missisipi','River Chad'], answer:1}
+
+            {q:'1. What is the longest river in Africa?', options: ['River Niger','River Nile','River Missisipi','River Chad'], answer:1}
+
+            {q:'1. What is the longest river in Africa?', options: ['River Niger','River Nile','River Missisipi','River Chad'], answer:1}
+        ]
+    }
